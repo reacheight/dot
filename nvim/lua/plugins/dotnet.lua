fa7495ecda71555dd20fccb095de89a7e6f170e3
Roman Mendaliev <reacheight8@gmail.com>
2026-08-17 11:44:41 +0300

    chore(nvim): set easy-dotnet commit tag, remove catpuccin

--- nvim/lua/plugins/dotnet.lua
+++ nvim/lua/plugins/dotnet.lua
@@ -1,5 +1,6 @@
 return {
   "GustavEikaas/easy-dotnet.nvim",
+  commit = "5823c50d0de015c01f591319f6e846506518165e",
   dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
   config = function()
     require("easy-dotnet").setup({
